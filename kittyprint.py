5999420993f80ab96c5da3d29e66e2ff02c44d40
KarlTheCool <k@rlphilli.ps>
2021-10-30 02:28:11 -0700

    change from pyside to pyqt5

--- kittyprint.py
+++ kittyprint.py
@@ -1,10 +1,11 @@
 import sys
 import os.path
 import catte
-from PySide6.QtWidgets import (
+from PyQt5.QtWidgets import (
     QPlainTextEdit, QPushButton, QApplication, QVBoxLayout, QDialog,
-    QLabel, QScrollArea)
-from PySide6.QtGui import QPixmap
+    QLabel, QGraphicsView, QFrame, QAbstractScrollArea, QGraphicsScene, QGraphicsPixmapItem)
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt, QSize
 from PIL.ImageQt import ImageQt
 from PIL import Image, ImageDraw, ImageFont
 import textwrap
@@ -18,41 +19,45 @@ class Form(QDialog):
         self.edit = QPlainTextEdit()
         self.edit.setPlaceholderText("Write text here")
         self.edit.textChanged.connect(self.update_preview)
-        self.edit.setStyleSheet("QPlainTextEdit { min-height: 15em }")
+        self.edit.cursorPositionChanged.connect(self.update_preview)
+        self.edit.setStyleSheet("QPlainTextEdit { min-height: 15em; }")
 
-        self.pic = QLabel()
+        self.pic = QGraphicsView()
+        self.pic.setStyleSheet("QGraphicsView { min-height: 15em }")
+        self.pic.setSizeIncrement(QSize(0, 0))
+        self.pic.setFrameShadow(QFrame.Raised)
+        self.pic.setSizeAdjustPolicy(
+            QAbstractScrollArea.AdjustToContents)
+        self.pic.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.pic.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.pic.setAlignment(Qt.AlignTop)
+        self.pic.setObjectName("pic")
         self.update_preview()
-
-        self.scrollArea = QScrollArea()
 
         self.button = QPushButton("Print!")
 
         # Create layout and add widgets
         layout = QVBoxLayout()
         layout.addWidget(self.edit)
-        layout.addWidget(self.scrollArea)
+        layout.addWidget(self.pic)
         layout.addWidget(self.button)
-
-        self.scrollArea.setWidget(self.pic)
-        self.scrollArea.setWidgetResizable(True)
-        self.scrollArea.setStyleSheet("QScrollArea { min-height: 15em }")
 
         # Set dialog layout
         self.setLayout(layout)
-        self.setStyleSheet("QVBoxLayout { min-width: 400px }")
+        self.setStyleSheet("QDialog { min-width: 400px }")
 
         # Add button signal to greetings slot
         self.button.clicked.connect(self.save_image)
 
     def generate_image(self):
         # Clear Image
-        im = Image.new(mode="RGB", size=(324, 1),
-                       color=(255, 255, 255))
+        im = Image.new(mode="RGBA", size=(324, 1),
+                       color=(255, 255, 255, 255))
 
         # Draw Text
         script_path = os.path.abspath(os.path.dirname(__file__))
         font_path = os.path.join(script_path, 'RobotoMono.ttf')
-        font = ImageFont.truetype(font=font_path, size=16)
+        font = ImageFont.truetype(font=font_path, size=24)
         ascent, descent = font.getmetrics()
         (font_width, baseline) = font.getsize('0')
         line_height = ascent + descent
@@ -66,7 +71,7 @@ class Form(QDialog):
         for p in paragraphs:
             lines += textwrap.wrap(
                 p,
-                width=int(im.size[0] / font_width),
+                width=int(im.size[0] / font_width - 1),
                 replace_whitespace=False)
 
         lineCount = len(lines)
@@ -88,12 +93,19 @@ class Form(QDialog):
         return im
 
     def update_preview(self):
+        # Update preview image
         im = self.generate_image()
+        scene = QGraphicsScene(self)
+        pixmap = QPixmap.fromImage(ImageQt(im))
+        item = QGraphicsPixmapItem(pixmap)
+        scene.addItem(item)
+        self.pic.setScene(scene)
 
-        # Show image in widget
-        qim = ImageQt(im)
-        pix = QPixmap.fromImage(qim)
-        self.pic.setPixmap(pix)
+        # Scroll to match vertical position of cursor
+        cursor_line = self.edit.textCursor().block().firstLineNumber()
+        line_count = self.edit.document().blockCount()
+        scroll_height = self.pic.verticalScrollBar().maximum()
+        self.pic.verticalScrollBar().setValue(cursor_line / line_count * scroll_height)
 
     # Greets the user
     def save_image(self):
